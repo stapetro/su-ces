03f179196696ba361e73692c3dbd2516e0fbc069
stanislav.i.petrov <stanislav.i.petrov@b276b64f-9c26-d8b3-9e73-0bf544f7073e>
2012-03-30 23:20:15 +0000

    trying to make model project to work...

--- su-ces/su-ces-model/src/main/java/bg/sofia/uni/fmi/ces/model/user/Role.java
+++ su-ces/su-ces-model/src/main/java/bg/sofia/uni/fmi/ces/model/user/Role.java
@@ -1,10 +1,14 @@
 package bg.sofia.uni.fmi.ces.model.user;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -24,7 +28,54 @@ public class Role {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="role_id")
 	private Long id;
-	@Column(name="role_name")
+	@Column(name="role_name", length=128)
 	private String name;
 	private String description;
+	@ManyToMany(mappedBy="roles")
+	private Collection<User> users;
+	
+	public Role() {
+		this.users = new ArrayList<>();
+	}
+	
+	public Role(String name, String desc) {
+		this();
+		this.name = name;
+		this.description = desc;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Collection<User> getUsers() {
+		return users;
+	}
+	public void setUsers(Collection<User> users) {
+		this.users = users;
+	}
+	
+	public void addUser(User user) {
+		if(this.users.contains(user) == false) {
+			this.users.add(user);
+		}
+		Collection<Role> userRoles = user.getRoles();
+		if(userRoles.contains(this) == false) {
+			userRoles.add(this);
+		}
+	}
 }
--- su-ces/su-ces-model/src/main/java/bg/sofia/uni/fmi/ces/model/user/User.java
+++ su-ces/su-ces-model/src/main/java/bg/sofia/uni/fmi/ces/model/user/User.java
@@ -1,33 +1,109 @@
 package bg.sofia.uni.fmi.ces.model.user;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 /**
  * Represents user.
+ * 
  * @author Staskata
- *
+ * 
  */
 @Entity
-@Table(
-		name="users",
-        uniqueConstraints=
-            @UniqueConstraint(columnNames={"email"})
-    )
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = { "email" }))
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="user_id")
+	@Column(name = "user_id")
 	private Long id;
 	private String email;
+	@Column(length = 128)
 	private String password;
-	@Column(name="create_date")
-	private java.sql.Date createDate;
-	@Column(name="last_login_date")
-	private java.sql.Date lastLoginDate;
+	@Column(name = "create_date", nullable=false)
+	private java.sql.Timestamp createDate;
+	@Column(name = "last_login_date")
+	private java.sql.Timestamp lastLoginDate;
+	@ManyToMany
+	private Collection<Role> roles;
+	
+	public User() {
+		this.roles = new ArrayList<>();
+	}
+	
+	public User(String email, String password) {
+		this();
+		this.email = email;
+		this.password = password;
+		long todayInMillis = Calendar.getInstance().getTimeInMillis();
+		this.createDate = new Timestamp(todayInMillis);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public java.sql.Timestamp getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(java.sql.Timestamp createDate) {
+		this.createDate = createDate;
+	}
+
+	public java.sql.Timestamp getLastLoginDate() {
+		return lastLoginDate;
+	}
+
+	public void setLastLoginDate(java.sql.Timestamp lastLoginDate) {
+		this.lastLoginDate = lastLoginDate;
+	}
+
+	public Collection<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<Role> roles) {
+		this.roles = roles;
+	}
+
+	public void addRole(Role role) {
+		if (this.roles.contains(role) == false) {
+			this.roles.add(role);
+		}
+		 Collection<User> users = role.getUsers();
+		if (users.contains(this) == false) {
+			users.add(this);
+		}
+	}
 }
