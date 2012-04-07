25da703681379617e9ae184477ef2510af517261
stanislav.i.petrov <stanislav.i.petrov@b276b64f-9c26-d8b3-9e73-0bf544f7073e>
2012-04-07 23:31:51 +0000

    SucesConfigurator enhanced

--- trunk/su-ces/su-ces-web/src/main/java/bg/sofia/uni/fmi/ces/conf/SucesConfigurator.java
+++ trunk/su-ces/su-ces-web/src/main/java/bg/sofia/uni/fmi/ces/conf/SucesConfigurator.java
@@ -8,50 +8,68 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 public class SucesConfigurator {
-	
+
 	private Logger logger;
 	private Properties properties;
 	private static SucesConfigurator sucesConfigurator;
-	
+
 	public static String getValue(String key) {
-		if(sucesConfigurator == null) {
+		if (sucesConfigurator == null) {
 			sucesConfigurator = new SucesConfigurator();
 		}
 		return sucesConfigurator.getPropValue(key);
 	}
-	
+
 	private SucesConfigurator() {
+	}
+
+	private String getPropValue(String key) {
+		InputStream confInputStream = loadProps();
+		if (confInputStream != null) {
+			try {
+				if(this.properties == null) {
+					return "";
+				}
+				if (key == null || key.isEmpty()) {
+					getLogger().error("Key is NULL or empty");
+					return "";
+				}
+				String value = this.properties.getProperty(key);
+				if (value == null || value.isEmpty()) {
+					getLogger().error("Value for key '" + key + "' not found");
+					return "";
+				}
+				return value;
+			} finally {
+				try {
+					confInputStream.close();
+				} catch (IOException e) {
+					getLogger().error(e);
+				}
+			}
+		}
+		return "";
+	}
+
+	private Logger getLogger() {
+		if (this.logger == null) {
+			this.logger = LogManager.getLogger(SucesConfigurator.class);
+		}
+		return this.logger;
+	}
+
+	private InputStream loadProps() {
 		ClassLoader classLoader = this.getClass().getClassLoader();
 		final String CONFIG_FILE = "conf/sucesweb.properties";
-		InputStream confInputStream = classLoader.getResourceAsStream(CONFIG_FILE);
+		InputStream confInputStream = classLoader
+				.getResourceAsStream(CONFIG_FILE);
 		this.properties = new Properties();
 		try {
 			this.properties.load(confInputStream);
 		} catch (IOException e) {
-			getLogger().fatal("Sucesweb config file '" + CONFIG_FILE + "' not found");
+			getLogger().fatal(
+					"Suces web config file '" + CONFIG_FILE + "' not found");
 		}
-	}
-	
-	private String getPropValue(String key) {
-		if(this.properties == null) {
-			return "";
-		}
-		if(key == null || key.isEmpty()) {
-			getLogger().error("Key is NULL or empty");
-			return "";
-		}
-		String value = this.properties.getProperty(key);
-		if(value == null || value.isEmpty()) {
-			getLogger().error("Value for key '" + key + "' not found");
-			return "";
-		}
-		return value;
-	}
-	
-	private Logger getLogger() {
-		if(this.logger == null) {
-			this.logger = LogManager.getLogger(SucesConfigurator.class);
-		}
-		return this.logger;
+		return confInputStream;
 	}
 }
