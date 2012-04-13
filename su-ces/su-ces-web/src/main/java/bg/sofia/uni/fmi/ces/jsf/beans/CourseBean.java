f80dcc879dccc80bedea553af32b7b79a6307e71
kbailov@gmail.com <kbailov@gmail.com@b276b64f-9c26-d8b3-9e73-0bf544f7073e>
2012-04-13 15:44:13 +0000

    Implement course feedback persisting to DB

---
+++ su-ces/su-ces-web/src/main/java/bg/sofia/uni/fmi/ces/jsf/beans/CourseBean.java
@@ -0,0 +1,10 @@
+package bg.sofia.uni.fmi.ces.jsf.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="courseBean")
+@SessionScoped
+public class CourseBean {
+
+}
---
+++ su-ces/su-ces-web/src/main/java/bg/sofia/uni/fmi/ces/session/utils/SessionUtils.java
@@ -0,0 +1,59 @@
+package bg.sofia.uni.fmi.ces.session.utils;
+
+import java.util.Map;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+import bg.sofia.uni.fmi.ces.constants.BeanNames;
+import bg.sofia.uni.fmi.ces.jsf.beans.SessionBean;
+
+/**
+ * This class provided a basic mechanism for accessing and working with session
+ * objects.
+ */
+public class SessionUtils {
+
+	/**
+	 * Provide a reference to an object held in the session of the application.
+	 * 
+	 * @param name
+	 *            name of the object
+	 * @return the found object. If the name is NULL or empty then NULL will be
+	 *         returned. If no object is found NULL will be returned.
+	 */
+	public static Object getSessionObject(String name) {
+		if (name == null || name.trim().length() == 0) {
+			return null;
+		}
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = facesContext.getExternalContext();
+		Map<String, Object> session = externalContext.getSessionMap();
+
+		// TODO remove the session map dump
+		for (Map.Entry<String, Object> entry : session.entrySet()) {
+			System.out.println("-- " + entry.getKey() + " - "
+					+ entry.getValue());
+		}
+
+		Object sessionObject = session.get(name);
+		return sessionObject;
+	}
+
+	/**
+	 * Provide the username of the user logged into the current session
+	 * 
+	 * @return the corresponding username. NULL if the name could not be found.
+	 */
+	public static String getLoggedUserName() {
+		Object sessionObject = getSessionObject(BeanNames.SESSION_BEAN);
+		if (sessionObject != null && sessionObject instanceof SessionBean) {
+			SessionBean sessionBean = (SessionBean) sessionObject;
+			String userName = sessionBean.getUserName();
+			return userName;
+		}
+
+		return null;
+	}
+}
