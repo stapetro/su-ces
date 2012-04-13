792c485a31b8e2ca581f4742941fb6b68925c40f
kbailov@gmail.com <kbailov@gmail.com@b276b64f-9c26-d8b3-9e73-0bf544f7073e>
2012-04-13 15:42:09 +0000

    Implement a facade for using common model operations like "persist"

---
+++ su-ces/su-ces-model/src/main/java/bg/sofia/uni/fmi/ces/model/facade/ModelFacade.java
@@ -0,0 +1,30 @@
+package bg.sofia.uni.fmi.ces.model.facade;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class ModelFacade {
+
+	public void persist(Object object) {
+		if (object == null) {
+			return;
+		}
+
+		EntityManagerFactory emFactory = Persistence
+				.createEntityManagerFactory("sucesPU");
+		EntityManager entityMgr = emFactory.createEntityManager();
+		EntityTransaction transaction = entityMgr.getTransaction();
+		transaction.begin();
+
+		entityMgr.persist(object);
+
+		entityMgr.flush();
+
+		transaction.commit();
+		entityMgr.close();
+		emFactory.close();
+	}
+
+}
