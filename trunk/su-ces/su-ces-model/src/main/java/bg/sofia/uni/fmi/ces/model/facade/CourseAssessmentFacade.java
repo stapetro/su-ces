24c7658da3ead501a524cc4d4555968839fa31c1
kbailov@gmail.com <kbailov@gmail.com@b276b64f-9c26-d8b3-9e73-0bf544f7073e>
2012-04-13 19:54:42 +0000

    implement loading course assessments from the database based on the logged user

---
+++ trunk/su-ces/su-ces-model/src/main/java/bg/sofia/uni/fmi/ces/model/facade/CourseAssessmentFacade.java
@@ -0,0 +1,70 @@
+package bg.sofia.uni.fmi.ces.model.facade;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import bg.sofia.uni.fmi.ces.model.course.Course;
+import bg.sofia.uni.fmi.ces.model.course.CourseAssessment;
+
+/**
+ * Provide a common interface for working with course assessment specific
+ * operations.
+ */
+public class CourseAssessmentFacade {
+
+	/**
+	 * Query for selecting the course assessment results.
+	 */
+	private static final String SELECT_COURSE_ASSESSMENT_BY_USER_AND_COURSE_ID = "SELECT ca "
+			+ "FROM CourseAssessment ca "
+			+ "WHERE ca.usersUserEmail = :userName "
+			+ "AND ca.cours.courseId = :courseId";
+
+	/**
+	 * Find the course assessment records based on the provided user name and
+	 * course information
+	 * 
+	 * @param userName
+	 *            user name of the user who uploaded the course assessment
+	 * @param course
+	 *            course that was assessed
+	 * @return the corresponding records. NULL if the input parameters are
+	 *         invalid or no record is found
+	 */
+	public CourseAssessment getCourseAssassment(String userName, Course course) {
+		if (userName == null || userName.trim().length() == 0 || course == null) {
+			return null;
+		}
+
+		EntityManagerFactory emFactory = Persistence
+				.createEntityManagerFactory("sucesPU");
+		EntityManager entityMgr = emFactory.createEntityManager();
+
+		Query query = entityMgr
+				.createQuery(SELECT_COURSE_ASSESSMENT_BY_USER_AND_COURSE_ID);
+		int courseId = course.getCourseId();
+		query.setParameter("userName", userName);
+		query.setParameter("courseId", courseId);
+
+		List<CourseAssessment> resultList = (List<CourseAssessment>) query
+				.getResultList();
+
+		CourseAssessment courseAssessment = null;
+		if (resultList != null && resultList.size() == 1) {
+			// the result set is expected to contain a single record or no
+			// records at all
+			// otherwise something wrong has happened with the DB restrictions
+			courseAssessment = resultList.get(0);
+		}
+
+		entityMgr.close();
+		emFactory.close();
+
+		return courseAssessment;
+	}
+
+}
