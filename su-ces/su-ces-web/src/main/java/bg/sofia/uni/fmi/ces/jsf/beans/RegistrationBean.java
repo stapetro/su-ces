03218b44d73bd04a4773626628f52a751d235ff6
stanislav.i.petrov <stanislav.i.petrov@b276b64f-9c26-d8b3-9e73-0bf544f7073e>
2012-04-15 20:19:47 +0000

    Start implementing user registration feature.

---
+++ su-ces/su-ces-web/src/main/java/bg/sofia/uni/fmi/ces/jsf/beans/RegistrationBean.java
@@ -0,0 +1,24 @@
+package bg.sofia.uni.fmi.ces.jsf.beans;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name="registrationBean")
+@ViewScoped
+public class RegistrationBean implements Serializable {
+	
+	private static final long serialVersionUID = -1018989257462312837L;
+	
+	public String register() {
+		//TODO To be implemented
+		return "";
+	}
+	
+	public String cancel() {
+		//TODO To be implemented
+		return "";
+	}		
+
+}
