f4d22024bc5ae8a0b8a11e8b54cda703604c5d0b
kbailov@gmail.com <kbailov@gmail.com@b276b64f-9c26-d8b3-9e73-0bf544f7073e>
2012-04-30 15:29:37 +0000

    Implement course preview for students

---
+++ su-ces/su-ces-model/src/main/java/bg/sofia/uni/fmi/ces/model/facade/course/CoursePersistence.java
@@ -0,0 +1,55 @@
+package bg.sofia.uni.fmi.ces.model.facade.course;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Query;
+
+import bg.sofia.uni.fmi.ces.model.course.Course;
+import bg.sofia.uni.fmi.ces.model.facade.ModelFacade;
+
+/**
+ * Class providing basic set of methods for working with courses
+ */
+public class CoursePersistence extends ModelFacade implements Serializable {
+
+	private static final long serialVersionUID = 6029218129335321213L;
+
+	/**
+	 * Select statement for obtaining the <code>Course</code> object by its ID
+	 */
+	private static final String SELECT_COURSE_BY_COURSE_ID = "SELECT c "
+			+ "FROM Course c " + 
+			"WHERE c.courseId = :courseId";
+
+	public CoursePersistence() {
+		super();
+	}
+
+	/**
+	 * Get the course referenced by the provided course ID
+	 * 
+	 * @param courseId
+	 *            ID of the course to search for
+	 * @return the course matching the ID. If no course is present NULL will be
+	 *         returned
+	 */
+	public Course geCourseById(int courseId) {
+		if (courseId < 0) {
+			return null;
+		}
+
+		Query query = entityManager.createQuery(SELECT_COURSE_BY_COURSE_ID);
+		query.setParameter("courseId", courseId);
+
+		List<Course> courseList = (List<Course>) query.getResultList();
+
+		Course course = null;
+		if (courseList != null && courseList.size() == 1) {
+			course = courseList.get(0);
+		}
+
+		return course;
+	}
+
+}
--- su-ces/su-ces-web/src/main/java/bg/sofia/uni/fmi/ces/jsf/beans/CourseBean.java
+++ su-ces/su-ces-web/src/main/java/bg/sofia/uni/fmi/ces/jsf/beans/CourseBean.java
@@ -1,10 +1,36 @@
 package bg.sofia.uni.fmi.ces.jsf.beans;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 
-@ManagedBean(name="courseBean")
-@SessionScoped
-public class CourseBean {
+import bg.sofia.uni.fmi.ces.model.course.Course;
+import bg.sofia.uni.fmi.ces.model.facade.course.CoursePersistence;
 
+@ManagedBean(name = "courseBean")
+@ViewScoped
+public class CourseBean implements Serializable {
+
+	private static final long serialVersionUID = -1373151780844385607L;
+
+	private Course course;
+
+	private CoursePersistence coursePersistence;
+
+	public CourseBean() {
+		coursePersistence = new CoursePersistence();
+
+		// TODO getting course 1...when course searching is implemented this one
+		// should be finished
+		course = coursePersistence.geCourseById(1);
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
 }
