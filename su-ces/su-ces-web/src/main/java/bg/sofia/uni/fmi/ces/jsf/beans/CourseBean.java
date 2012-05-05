51be85075c5f6660cc0e31628a2e811df9b70cc5
kbailov@gmail.com <kbailov@gmail.com@b276b64f-9c26-d8b3-9e73-0bf544f7073e>
2012-05-05 19:30:13 +0000

    implement course update - puts the updates to the database

--- su-ces/su-ces-web/src/main/java/bg/sofia/uni/fmi/ces/jsf/beans/CourseBean.java
+++ su-ces/su-ces-web/src/main/java/bg/sofia/uni/fmi/ces/jsf/beans/CourseBean.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.RequestScoped;
 import javax.faces.event.ActionEvent;
 
 import bg.sofia.uni.fmi.ces.model.course.Course;
@@ -15,17 +15,50 @@ import bg.sofia.uni.fmi.ces.model.course.Specialty;
 import bg.sofia.uni.fmi.ces.model.facade.course.CoursePersistence;
 
 @ManagedBean(name = "courseBean")
-@ViewScoped
+@RequestScoped
 public class CourseBean implements Serializable {
 
 	private static final long serialVersionUID = -1373151780844385607L;
 
+	/**
+	 * Course reference object for working with course data
+	 */
 	private Course course;
-	
+
+	/**
+	 * List of all available semesters
+	 */
+	private List<Semester> semesterList;
+
+	/**
+	 * List of all available specialties
+	 */
+	private List<Specialty> specialtyList;
+
+	/**
+	 * List of all available grades
+	 */
+	private List<Grade> gradeList;
+
+	/**
+	 * List of the ID values for the selected specialties
+	 */
 	private List<String> selectedSpecialties;
-	
+
+	/**
+	 * List of the ID values of the selected grades
+	 */
 	private List<String> selectedGrades;
-	
+
+	/**
+	 * ID of the value for the selected semester
+	 */
+	private int selectedSemesterId;
+
+	/**
+	 * Reference to the course persistence object used to managed work with the
+	 * persistence entities and DB
+	 */
 	private CoursePersistence coursePersistence;
 
 	public CourseBean() {
@@ -36,36 +69,121 @@ public class CourseBean implements Serializable {
 		// TODO getting course 1...when course searching is implemented this one
 		// should be finished
 		course = coursePersistence.geCourseById(1);
-		if(course != null){
-			for(Specialty specialty : course.getSpecialties()){
+		if (course != null) {
+			for (Specialty specialty : course.getSpecialties()) {
 				selectedSpecialties.add("" + specialty.getSpecialtyId());
 			}
-			
-			for(Grade grade : course.getGrades()){
+
+			for (Grade grade : course.getGrades()) {
 				selectedGrades.add("" + grade.getGradeId());
 			}
+
+			selectedSemesterId = course.getSemester().getSemesterId();
 		}
 	}
-	
-	public void saveCourse(ActionEvent event){
-		System.out.println("-------- SAVING COURSE ---------");
+
+	/**
+	 * Action for saving course data
+	 * 
+	 * @param event
+	 */
+	public void saveCourse(ActionEvent event) {
+		List<Specialty> selectedSpecialtyList = getSpecialtyListByIds(selectedSpecialties);
+		course.setSpecialties(selectedSpecialtyList);
+
+		List<Grade> selectedGradesList = getGradesListByIds(selectedGrades);
+		course.setGrades(selectedGradesList);
+
+		Semester selectedSemester = getSemesterById(selectedSemesterId);
+		course.setSemester(selectedSemester);
+
+		coursePersistence.beginTransaction();
+		coursePersistence.persist(course);
+		coursePersistence.commitTransaction();
+
+		// course = coursePersistence.geCourseById(1);
 	}
-	
-	public List<Semester> getSemesterList(){
-		List<Semester> semesterList = coursePersistence.getSemesters();
-		
+
+	/**
+	 * Provide the list of Specialty object that refer to the selected ones
+	 * 
+	 * @param idList
+	 *            list of the id values for the selected specialties
+	 * @return the referring Specialties in list
+	 */
+	private List<Specialty> getSpecialtyListByIds(List<String> idList) {
+		List<Specialty> specialtyMatchingList = new LinkedList<Specialty>();
+
+		for (String stringId : idList) {
+			int id = Integer.parseInt(stringId);
+
+			for (Specialty specialty : specialtyList) {
+				if (id == specialty.getSpecialtyId()) {
+					specialtyMatchingList.add(specialty);
+				}
+			}
+		}
+
+		return specialtyMatchingList;
+	}
+
+	/**
+	 * Provide the list of Grade object that refer to the selected ones
+	 * 
+	 * @param idList
+	 *            list of the id values for the selected grades
+	 * @return the referring Grades in List
+	 */
+	private List<Grade> getGradesListByIds(List<String> idList) {
+		List<Grade> gradeMatchingList = new LinkedList<Grade>();
+
+		for (String stringId : idList) {
+			int id = Integer.parseInt(stringId);
+
+			for (Grade grade : gradeList) {
+				if (id == grade.getGradeId()) {
+					gradeMatchingList.add(grade);
+				}
+			}
+		}
+
+		return gradeMatchingList;
+	}
+
+	/**
+	 * Provide the Semester object the refers to the id of the selected one
+	 * 
+	 * @param id
+	 *            ID of the selected course
+	 * @return the corresponding Semester object
+	 */
+	private Semester getSemesterById(int id) {
+		for (Semester semester : semesterList) {
+			if (id == semester.getSemesterId()) {
+				return semester;
+			}
+		}
+
+		return null;
+	}
+
+	// ================ PROPERTY GETTERS AND SETTERS ===================
+
+	public List<Semester> getSemesterList() {
+		semesterList = coursePersistence.getSemesters();
+
 		return semesterList;
 	}
-	
-	public List<Specialty> getSpecialtyList(){
-		List<Specialty> specialtyList = coursePersistence.getSpecialties();
-		
+
+	public List<Specialty> getSpecialtyList() {
+		specialtyList = coursePersistence.getSpecialties();
+
 		return specialtyList;
 	}
-	
-	public List<Grade> getGradeList(){
-		List<Grade> gradeList = coursePersistence.getGrades();
-		
+
+	public List<Grade> getGradeList() {
+		gradeList = coursePersistence.getGrades();
+
 		return gradeList;
 	}
 
@@ -83,6 +201,14 @@ public class CourseBean implements Serializable {
 
 	public void setSelectedGrades(List<String> selectedGrades) {
 		this.selectedGrades = selectedGrades;
+	}
+
+	public int getSelectedSemesterId() {
+		return selectedSemesterId;
+	}
+
+	public void setSelectedSemesterId(int selectedSemesterId) {
+		this.selectedSemesterId = selectedSemesterId;
 	}
 
 	public Course getCourse() {
